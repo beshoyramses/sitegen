2d7d85e972ba0a0c6f0a526f412bcb2557879087
beshoy66q <98409221+beshoy66q@users.noreply.github.com>
2024-09-16 21:53:21 +0300

    fixing

--- src/components/media/media-card.tsx
+++ src/components/media/media-card.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { Media } from '@prisma/client'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 import {
@@ -12,7 +11,6 @@ import {
 } from '@/components/ui/dropdown-menu'
 import {
   AlertDialog,
-  AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
@@ -26,11 +24,9 @@ import Image from 'next/image'
 // import { deleteMedia, saveActivityLogsNotification } from '@/lib/queries'
 // import { toast } from '../ui/use-toast'
 
-type Props = { file: Media }
+type Props = { file: any }
 
 const MediaCard = ({ file }: Props) => {
-  const [loading, setLoading] = useState(false)
-  const router = useRouter()
 
   return (
     <AlertDialog>
@@ -60,15 +56,8 @@ const MediaCard = ({ file }: Props) => {
           <DropdownMenuContent>
             <DropdownMenuLabel>Menu</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className="flex gap-2"
-              onClick={() => {
-                navigator.clipboard.writeText(file.link)
-                toast({ title: 'Copied To Clipboard' })
-              }}
-            >
+           
               <Copy size={15} /> Copy Image Link
-            </DropdownMenuItem>
             <AlertDialogTrigger asChild>
               <DropdownMenuItem className="flex gap-2">
                 <Trash size={15} /> Delete File
@@ -89,27 +78,7 @@ const MediaCard = ({ file }: Props) => {
         </AlertDialogHeader>
         <AlertDialogFooter className="flex items-center">
           <AlertDialogCancel className="mb-2">Cancel</AlertDialogCancel>
-          <AlertDialogAction
-            disabled={loading}
-            className="bg-destructive hover:bg-destructive"
-            onClick={async () => {
-              setLoading(true)
-              const response = await deleteMedia(file.id)
-              await saveActivityLogsNotification({
-                agencyId: undefined,
-                description: `Deleted a media file | ${response?.name}`,
-                subaccountId: response.subAccountId,
-              })
-              toast({
-                title: 'Deleted File',
-                description: 'Successfully deleted the file',
-              })
-              setLoading(false)
-              router.refresh()
-            }}
-          >
-            Delete
-          </AlertDialogAction>
+         
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
